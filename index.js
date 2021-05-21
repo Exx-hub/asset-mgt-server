19cd2f24bcf87ea9c435b681553c65fb60731537
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-21 21:56:33 +0800

    update: Connect database and initial on index js

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_CONN_STRING, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error: "));
+db.once("open", () => console.log("Db connection established!"));
 
 const PORT = process.env.PORT;
 
@@ -10,4 +21,4 @@ app.get("/", (req, res) => {
 	res.json({ message: "ROOT route for Asset Mgt System" });
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(3000, () => console.log(`Server listening on port ${PORT}`));
