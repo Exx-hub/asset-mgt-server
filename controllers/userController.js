4e43eb6211bd0a0e6f81beace93612f4348b46fb
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-24 20:35:12 +0800

    Install bcrypt, Setup register function and login function in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,51 @@
-const register = (req, res) => {
-	res.json({ message: "register" });
+const User = require("../models/userModel");
+
+const bcrypt = require("bcrypt");
+// used to encrpyt data like passwords
+
+const register = async (req, res) => {
+	let { firstName, lastName, emailAddress, password } = req.body;
+
+	password = bcrypt.hashSync(password, 10);
+
+	let newUser = new User({
+		firstName,
+		lastName,
+		emailAddress,
+		password,
+		transactions: [],
+		assets: [],
+	});
+
+	try {
+		const savedUser = await newUser.save();
+
+		res.json({
+			message: "Save Successful",
+			data: savedUser,
+		});
+	} catch (err) {
+		res.status(400).send("Error saving user!");
+	}
 };
 
-const login = (req, res) => {
-	res.json({ message: "login" });
+const login = async (req, res) => {
+	const email = req.body.emailAddress;
+	const password = req.body.password;
+
+	const foundUser = await User.findOne({ emailAddress: email }, "_id");
+
+	if (!foundUser) {
+		res.send({
+			userDetails: "Not found",
+			message: "Username not registerd",
+		});
+	} else {
+		res.send({
+			userDetails: foundUser,
+			message: "This is the user",
+		});
+	}
 };
 
 const details = (req, res) => {
